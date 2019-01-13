3be74227bb052f0880af45e55e5c76b409d1f266
Batuhan Taşkaya <batuhanosmantaskaya@gmail.com>
2019-01-13 15:17:19 +0300

    Pipfile and devtools
    Pipfile added for dev packages (like astor, astpretty, pytest)
    Some tools created for debugging. In xmllang/bin directory.
    
    Tools;
    to_source XMLFILE.xml -> Converts your xmlfile to python ast then uses
    astor.code_gen.to_source to convert it back to python
    
    to_pyast XMLFILE.xml -> Converts your xmlfile to python ast then uses
    astpretty.pprint for printing it

---
+++ xmllang/bin/to_pyast.py
@@ -0,0 +1,12 @@
+from astpretty import pprint
+from xmllang.parser import Parser
+
+
+def main(fname):
+    p = Parser.fromfile(fname)
+    m = p.parse()
+    pprint(m)
+    
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1])
---
+++ xmllang/bin/to_source.py
@@ -0,0 +1,12 @@
+from astor.code_gen import to_source
+from xmllang.parser import Parser
+
+
+def main(fname):
+    p = Parser.fromfile(fname)
+    m = p.parse()
+    print(to_source(m))
+    
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1])
